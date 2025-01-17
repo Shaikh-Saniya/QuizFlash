38fb20b1e1f74cfdf526ab2eaf9b6a0f53e5d8eb
Shaikh Saniya <shaikhsaniya1001@gmail.com>
2025-01-17 17:26:43 +0530

    Initial commit

---
+++ project/android/app/src/main/kotlin/com/bryancorder/study_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.bryancorder.study_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    // No additional code required unless needed
+}
